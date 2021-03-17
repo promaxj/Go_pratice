75b28fabb49656fa2c3db7273fc8f2877dad12d4
zelig <Zelig.Shen@quantatw.com>
2021-03-17 19:58:04 +0800

    hello_world

---
+++ 1_hello_world_1.go
@@ -0,0 +1,11 @@
+// set current package
+package main
+
+// import some package
+import "fmt"
+
+// enter main function
+func main() {
+	// print out string "Hello World" on stand output.
+	fmt.Println("Hello World")
+}
